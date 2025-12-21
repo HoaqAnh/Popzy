30321a15103a7467189eb1654533fbdbd9416086
Nguyen Toan Thang <xamthang09@gmail.com>
2025-12-21 11:33:38 +0700

    add docs api

---
+++ src/main/java/com/propzy/propzy/config/SwaggerConfig.java
@@ -0,0 +1,19 @@
+package com.propzy.propzy.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI openAPI() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Eflex API")
+                        .description("API tài liệu cho hệ thống học tập Eflex")
+                        .version("1.0.0"));
+    }
+}
